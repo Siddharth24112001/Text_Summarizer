990f4597875dca72f1bd831f4e2151559d2c98f4
Sid <siddharth.dodia@ku.edu>
2023-10-16 10:35:20 -0500

    Updated

---
+++ src/textSummarizer/utils/common.py
@@ -0,0 +1,34 @@
+import os
+from box.exceptions import BoxException
+import yaml
+import textSummarizer.logging as logger
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+
+# The decorator will make sure that the argumenrts strictly follow the mentioned type
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+   try:
+      with open(path_to_yaml) as yaml_file:
+         content = yaml.safe_load(yaml_file)
+         logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+         return ConfigBox(content)
+   except BoxException:
+      raise ValueError("yaml file is empty")
+   except Exception as e:
+      raise e
+   
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose = True) :
+   for path in path_to_directories:
+      os.makedirs(path, exist_ok= True)
+      if verbose:
+         logger.info(f"created directory: {path}")
+
+
+@ensure_annotations 
+def get_size(path: Path) -> str:
+   size_in_kb = round(os.path.getsize(path) / 1024)
+   return f"{size_in_kb} KB"
